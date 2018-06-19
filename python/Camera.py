0a2457c9d7f44b715b51a665b01ea32a70095738
rteddylee62 <rteddylee62@hotmail.com>
2018-06-19 12:13:04 -0400

    Updated Camera class

--- python/Camera.py
+++ python/Camera.py
@@ -12,21 +12,30 @@ FILENAME_BASE = "photo_"
 
 class Camera:
 
-    def __init__(self):
+    def __init__( self, folderBase, filenameBase ):
         # initialize class variables
         self.camera = PiCamera()
+	self.camera.hflip = True
+	self.camera.vflip = True
 
-    def takePicture(self):
+	self.folderBase = folderBase
+	self.filenameBase = filenameBase
+
+    def close( self ):
+
+	self.camera.close()
+
+    def takePicture( self ):
         # take picture
         now = datetime.datetime.now()
-        filename = "%s%s%d%02d%02d_%02d%02d%02d.jpg" % (FOLDER_BASE, FILENAME_BASE, now.year, now.month, now.day, now.hour, now.minute, now.second)
+        filename = "%s/%s%d%02d%02d_%02d%02d%02d.jpg" % (self.folderBase, self.filenameBase, now.year, now.month, now.day, now.hour, now.minute, now.second)
         
         self.camera.capture( filename )
 
 class CameraTest:
 
 	def runTest( self ):
-		camera = Camera()
+		camera = Camera( FOLDER_BASE, FILENAME_BASE )
 		camera.takePicture()
 
 #//////////////////////////////////////////////////////////////////////////////
